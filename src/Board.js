9976862b4053ea0128ad193f41c7f5137623ec04
Derick Dsilva <mstrfpts@gmail.com>
2020-10-17 14:34:45 +0100

    Added a basic task object structure as a starting point to test task addition

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
+import { useLocalStorage } from "./CustomHooks";
 import TaskModal from "./TaskModal";
 
 const Board = () => {
   const [showModal, setShowModal] = useState(false);
+  const [taskList, setTaskList] = useLocalStorage("taskList", []); //useState([]);
   const handleAddTask = () => {
     setShowModal(true);
   };
+
+  const AddTask = (task) => {
+    let newTask = {
+      ...task,
+      id: findFreeId(taskList),
+    };
+    setTaskList([...taskList, newTask]);
+  };
+
+  const findFreeId = (array) => {
+    const sortedArray = array.slice().sort(function (a, b) {
+      return a.id - b.id;
+    });
+    let previousId = 0;
+    for (let element of sortedArray) {
+      if (element.id !== previousId + 1) {
+        return previousId + 1;
+      }
+      previousId = element.id;
+    }
+    return previousId + 1;
+  };
+
   return (
     <div className={"RootContainer"}>
       <div>
@@ -16,7 +41,11 @@ const Board = () => {
           +
         </button>
       </div>
-      <TaskModal showModal={showModal} setShowModal={setShowModal} />
+      <TaskModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        addTask={AddTask}
+      />
     </div>
   );
 };
